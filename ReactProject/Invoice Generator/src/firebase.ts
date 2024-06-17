087fa93395091bcb042a5fea7b23d49b49e0489e
Divu <divyeshkuchhadia@gmail.com>
2024-06-17 23:13:03 +0530

    done first test

--- ReactProject/Invoice Generator/src/firebase.ts
+++ ReactProject/Invoice Generator/src/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyDPE_dUm2RYer04pqpWHqbU_k6858dFl1M",
+    authDomain: "invoice-generator-e78ac.firebaseapp.com",
+    projectId: "invoice-generator-e78ac",
+    storageBucket: "invoice-generator-e78ac.appspot.com",
+    messagingSenderId: "734497770069",
+    appId: "1:734497770069:web:7d2a7f47e98814a95962dc",
+    measurementId: "G-EV17VXN0VY"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
