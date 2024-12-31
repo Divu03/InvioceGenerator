6c7380702539f28a3c1c90080e3096dc54d57deb
Divu <divyeshkuchhadia@gmail.com>
2024-12-31 23:18:57 +0530

    want to restart

--- src/components/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { auth } from '../firebase';
-
-const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return auth.currentUser ? <>{children}</> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
